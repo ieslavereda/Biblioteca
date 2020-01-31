b419b85ae541c90f676137a70372fec23e471b5c
jalonso <jalonso@administrador-ALDA>
2020-01-31 18:35:43 +0100

    a

--- src/main/Biblioteca.java
+++ src/main/Biblioteca.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.Scanner;
 
+import classes.Libro;
 import classes.Lista;
 
 public class Biblioteca {
@@ -13,14 +14,60 @@ public class Biblioteca {
 		Scanner entrada = new Scanner(System.in);
 		
 		do {
+			showMenu();
+			option=entrada.nextInt();
 			
-		}
-		
-		
-		
-
+			switch(option) {
+			case 0:System.out.println("Bye!!");
+				break;				
+			case 1:addBook(libros);
+				break;
+			case 2:deleteBook(libros);
+				break;
+			case 3:showBook(libros);
+				break;
+			case 4:showBooks(libros);
+				break;
+			default:System.out.println("Opcion no valida");
+					break;
+			}			
+			
+		}while(option!=0);
 	}
 	
+	private static void showBooks(Lista libros) {
+		
+		System.out.println(libros);
+		
+	}
+
+	private static void showBook(Lista libros) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void deleteBook(Lista libros) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void addBook(Lista libros) {
+		
+		Scanner entrada = new Scanner(System.in);
+		Libro libro;
+		String titulo,autor;
+		
+		System.out.println("Dame el titulo:");
+		titulo=entrada.nextLine();
+		System.out.println("Dame el autor:");
+		autor=entrada.nextLine();
+		
+		libro=new Libro(titulo,autor);
+		
+		libros.addElementHead(libro);
+		System.out.println("Libro añadido.");
+	}
+
 	public static void showMenu() {
 		
 		System.out.println("1. Introducir Libro");
@@ -29,7 +76,9 @@ public class Biblioteca {
 		System.out.println("4. Mostrar Libros");
 		System.out.println("-------------------");
 		System.out.println("0. Salir");
-		
+		System.out.println("-------------------");
+		System.out.println();
+		System.out.println("Introduzca opcion:");
 	}
 
 }
